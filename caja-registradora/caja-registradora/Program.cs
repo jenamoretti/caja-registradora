f594c1eccb10c853836328cb747bacebe42cbb8c
jenamoretti <jenaromoretti2005@gmail.com>
2026-08-11 20:13:56 -0300

    etapa 3: carga multiple con menu

--- caja-registradora/caja-registradora/Program.cs
+++ caja-registradora/caja-registradora/Program.cs
@@ -1,7 +1,19 @@
 ﻿using System.Runtime.CompilerServices;
 
 Kiosk kiosk = new Kiosk();
-kiosk.addProduct();
+kiosk.OpenCashRegister();
+kiosk.menu();
+
+class Product
+{
+    public string name;
+    public decimal price;
+    public Product(string name, decimal price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+}
 class Kiosk
 {
     public void OpenCashRegister()
@@ -17,13 +29,56 @@ class Kiosk
 
     }
 
-    public void addProduct()
+    public Product addProduct()
     {
         Console.WriteLine("Ingrese el nombre del producto:");
         string productName = Console.ReadLine();
         Console.WriteLine("Ingrese el precio del producto:");
         decimal productPrice = Convert.ToDecimal(Console.ReadLine());
         Console.WriteLine("Producto agregado: " + productName + " - Precio: $" + productPrice);
+
+        Product product = new Product(productName, productPrice);
+
+        return product;
+    }
+
+    public void menu()
+    {
+        bool open = true;
+        List<Product> cart = new List<Product>();
+
+        while(open)
+        {
+            Console.WriteLine("Que desea hacer?");
+            Console.WriteLine("1 - Cargar un producto");
+            Console.WriteLine("2 - Cerrar la venta");
+            int option = int.Parse(Console.ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    Product newProduct = addProduct();
+                    cart.Add(newProduct);
+                    break;
+
+                case 2:
+                    decimal total = 0;
+                    Console.WriteLine("\n=== RESUMEN DE VENTA ===");
+                    foreach (Product p in cart)
+                    {
+                        Console.WriteLine($"- {p.name}: ${p.price}");
+                        total += p.price;
+                    }
+                    Console.WriteLine($"Total a pagar: ${total}");
+
+                    open = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no válida.");
+                    break;
+            }
+        }
     }
 }
 

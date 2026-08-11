42428b3e3ed0d271a38db5d03e2f9a7f2b66f748
jenamoretti <jenaromoretti2005@gmail.com>
2026-08-11 19:55:25 -0300

    etapa 2: carga de un producto

--- caja-registradora/caja-registradora/Program.cs
+++ caja-registradora/caja-registradora/Program.cs
@@ -1,10 +1,30 @@
 ﻿using System.Runtime.CompilerServices;
 
-const string kioskName = "El Recreo";
+Kiosk kiosk = new Kiosk();
+kiosk.addProduct();
+class Kiosk
+{
+    public void OpenCashRegister()
+    {
+        const string kioskName = "El Recreo";
 
-Console.WriteLine("Ingrese el nombre del cajero:");
-string cashierName = Console.ReadLine();
+        Console.WriteLine("Ingrese el nombre del cajero:");
+        string cashierName = Console.ReadLine();
 
-Console.WriteLine("===KIOSCO" + kioskName.ToUpper() + "===");
-Console.WriteLine("Nombre del cajero: " + cashierName);
-Console.WriteLine("Bienvenido al sistema, " + cashierName + ". Caja abierta.");
+        Console.WriteLine("===KIOSCO" + kioskName.ToUpper() + "===");
+        Console.WriteLine("Nombre del cajero: " + cashierName);
+        Console.WriteLine("Bienvenido al sistema, " + cashierName + ". Caja abierta.");
+
+    }
+
+    public void addProduct()
+    {
+        Console.WriteLine("Ingrese el nombre del producto:");
+        string productName = Console.ReadLine();
+        Console.WriteLine("Ingrese el precio del producto:");
+        decimal productPrice = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Producto agregado: " + productName + " - Precio: $" + productPrice);
+    }
+}
+
+
